234efeae3e8346288ddfb699959ee8578d69f30e
Gigi Gamao <mmgamao1@gmail.com>
2019-11-26 21:16:23 +0000

    Automatically backed up by Learn

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,11 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+class Application < Rails::Application
+  config.web_console.whitelisted_ips = '<IP address here>'
+end
+
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
